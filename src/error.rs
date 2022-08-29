9c475f26196a3f1e32106d7bbc8097bc44f0d80a
Kristoffer Andersson <kristoffer.andersson@gu.se>
2022-08-29 22:58:08 +0200

    feat: use clap to parse line

--- src/error.rs
+++ src/error.rs
@@ -1,6 +1,26 @@
-#[derive(Debug, Clone, thiserror::Error)]
+use std::{error, fmt, io};
+#[derive(Debug)]
 pub enum Error {
-    #[error("Unknown error: {0}")]
+    InvalidQuoting(String),
+    Io(io::Error),
     Unknown(String),
 }
 pub type Result<T> = std::result::Result<T, Error>;
+
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            Error::InvalidQuoting(ref err) => write!(f, "Invalid quoting: '{}'", err),
+            Error::Io(ref err) => write!(f, "IO error: {}", err),
+            Error::Unknown(ref err) => write!(f, "Unknown error: {}", err),
+        }
+    }
+}
+
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod app;
 mod error;
 use rustyline::error::ReadlineError;
-
+use error::Error;
+use std::io::Write;
 fn main() -> rustyline::Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("trace"))
         .format_timestamp(None)
@@ -12,16 +13,20 @@ fn main() -> rustyline::Result<()> {
     loop {
         let readline = repl.readline(">> ");
         match readline {
-            Ok(command) => {
-                log::trace!("Line: {:?}", command);
-                match app::handle_command(command.clone()) {
-                    Ok(result) => {
+            Ok(line) => {
+                log::trace!("Line: {:?}", line);
+                match respond(&line) {
+                    Ok(quit) => {
                         log::trace!("command succedded");
-                        println!("{}", result);
+
+                        if quit {
+                            log::info!("quiting ...");
+                            break;
+                        }
                     }
                     Err(err) => {
                         log::error!("error: {:?}", err);
-                        eprintln!("command '{}' failed", command);
+                        println!("command '{}' failed with error: {}", line, err);
                     }
                 }
             }
@@ -40,4 +45,63 @@ fn main() -> rustyline::Result<()> {
         }
     }
     Ok(())
+}
+
+
+fn respond(line: &str) -> Result<bool, Error> {
+    let args = shlex::split(line).ok_or_else(|| Error::InvalidQuoting(line.to_string()))?;
+    let matches = cli()
+        .try_get_matches_from(&args)
+        .map_err(|e| Error::Unknown(e.to_string()))?;
+    match matches.subcommand() {
+        Some(("ping", _matches)) => {
+            writeln!(std::io::stdout(), "Pong")?;
+            std::io::stdout().flush()?;
+        }
+        Some(("quit", _matches)) => {
+            writeln!(std::io::stdout(), "Exiting ...")?;
+            std::io::stdout().flush()?;
+            return Ok(true);
+        }
+        Some((name, _matches)) => unimplemented!("{}", name),
+        None => unreachable!("subcommand required"),
+    }
+
+    Ok(false)
+
+}
+
+
+fn cli() -> clap::Command<'static> {
+    use clap::Command;
+    // strip out usage
+    const PARSER_TEMPLATE: &str = "\
+        {all-args}
+    ";
+    // strip out name/version
+    const APPLET_TEMPLATE: &str = "\
+        {about-with-newline}\n\
+        {usage-heading}\n    {usage}\n\
+        \n\
+        {all-args}{after-help}\
+    ";
+
+    Command::new("repl")
+        .multicall(true)
+        .arg_required_else_help(true)
+        .subcommand_required(true)
+        .subcommand_value_name("APPLET")
+        .subcommand_help_heading("APPLETS")
+        .help_template(PARSER_TEMPLATE)
+        .subcommand(
+            Command::new("ping")
+                .about("Get a response")
+                .help_template(APPLET_TEMPLATE),
+        )
+        .subcommand(
+            Command::new("quit")
+                .alias("exit")
+                .about("Quit the REPL")
+                .help_template(APPLET_TEMPLATE),
+        )
 }
